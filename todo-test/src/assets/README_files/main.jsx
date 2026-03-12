a8edc7a7b26d6b933f27a0d31f639b130ca88d70
jsbd29 <30559771+jsbd29@users.noreply.github.com>
2026-03-12 10:08:03 -0500

    adding screenshot

---
+++ todo-test/src/assets/README_files/main.jsx
@@ -0,0 +1,18 @@
+import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=46cd0c0c"; const jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_react from "/node_modules/.vite/deps/react.js?v=46cd0c0c"; const StrictMode = __vite__cjsImport1_react["StrictMode"];
+import __vite__cjsImport2_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=46cd0c0c"; const createRoot = __vite__cjsImport2_reactDom_client["createRoot"];
+import "/src/index.css";
+import Todo from "/src/components/Todo.jsx";
+createRoot(document.getElementById("root")).render(
+  /* @__PURE__ */ jsxDEV(StrictMode, { children: /* @__PURE__ */ jsxDEV(Todo, {}, void 0, false, {
+    fileName: "/Users/sebastiaodutra/Desktop/git-one/test10/todo-test/src/main.jsx",
+    lineNumber: 8,
+    columnNumber: 5
+  }, this) }, void 0, false, {
+    fileName: "/Users/sebastiaodutra/Desktop/git-one/test10/todo-test/src/main.jsx",
+    lineNumber: 7,
+    columnNumber: 3
+  }, this)
+);
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6IkFBT0k7QUFQSixTQUFTQSxrQkFBa0I7QUFDM0IsU0FBU0Msa0JBQWtCO0FBQzNCLE9BQU87QUFDUCxPQUFPQyxVQUFVO0FBRWpCRCxXQUFXRSxTQUFTQyxlQUFlLE1BQU0sQ0FBQyxFQUFFQztBQUFBQSxFQUMxQyx1QkFBQyxjQUNDLGlDQUFDLFVBQUQ7QUFBQTtBQUFBO0FBQUE7QUFBQSxTQUFLLEtBRFA7QUFBQTtBQUFBO0FBQUE7QUFBQSxTQUVBO0FBQ0YiLCJuYW1lcyI6WyJTdHJpY3RNb2RlIiwiY3JlYXRlUm9vdCIsIlRvZG8iLCJkb2N1bWVudCIsImdldEVsZW1lbnRCeUlkIiwicmVuZGVyIl0sImlnbm9yZUxpc3QiOltdLCJzb3VyY2VzIjpbIm1haW4uanN4Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IFN0cmljdE1vZGUgfSBmcm9tICdyZWFjdCdcbmltcG9ydCB7IGNyZWF0ZVJvb3QgfSBmcm9tICdyZWFjdC1kb20vY2xpZW50J1xuaW1wb3J0ICcuL2luZGV4LmNzcydcbmltcG9ydCBUb2RvIGZyb20gJy4vY29tcG9uZW50cy9Ub2RvLmpzeCdcblxuY3JlYXRlUm9vdChkb2N1bWVudC5nZXRFbGVtZW50QnlJZCgncm9vdCcpKS5yZW5kZXIoXG4gIDxTdHJpY3RNb2RlPlxuICAgIDxUb2RvIC8+XG4gIDwvU3RyaWN0TW9kZT4sXG4pXG4iXSwiZmlsZSI6Ii9Vc2Vycy9zZWJhc3RpYW9kdXRyYS9EZXNrdG9wL2dpdC1vbmUvdGVzdDEwL3RvZG8tdGVzdC9zcmMvbWFpbi5qc3gifQ==
